277c8e6765a6c6da1e83d47f3a4b90fff1de02c0
Stephen Glass <sglass520@gmail.com>
2020-07-11 22:39:20 -0400

    Fix text

--- server/routes/index.js
+++ server/routes/index.js
@@ -2,7 +2,7 @@ const express = require("express");
 const router = express.Router();
 
 router.get("/", (req, res) => {
-  res.send({ response: "I am alive" }).status(200);
+  res.send({ response: "OK" }).status(200);
 });
 
 module.exports = router;
